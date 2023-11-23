a6aa16e6e6bd4cbbd4828d46c516762a2aefa8e2
Salvador Domingues <salvadordomingues@gmail.com>
2023-11-23 12:28:01 +0000

    Revert "3 obsbtáculos em simultâneo"
    This reverts commit c3ad758f35a2f58efa6af0d08685c019bd5447ae.

--- src/game/ObstacleMover.java
+++ src/game/ObstacleMover.java
@@ -1,58 +1,35 @@
 package game;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import environment.Cell;
 import environment.LocalBoard;
 
 public class ObstacleMover extends Thread {
-    private static final Object lock = new Object(); // Objeto para sincronização
-    private static int movingCount = 0; // Contador de obstáculos em movimento
-    private static final int MAX_CONCURRENT_MOVES = 3; // Número máximo de obstáculos movidos simultaneamente
+	private Obstacle obstacle;
+	private LocalBoard board;
+	
+	public ObstacleMover(Obstacle obstacle, LocalBoard board) { // the one and only ThreadPool :)
+		super();
+		this.obstacle = obstacle;
+		this.board = board;
+	}
 
-    private Obstacle obstacle;
-    private LocalBoard board;
-
-    public ObstacleMover(Obstacle obstacle, LocalBoard board) { // the one and only ThreadPool :)
-        super();
-        this.obstacle = obstacle;
-        this.board = board;
-    }
-
-    @Override
-    public void run() {
-        while (true) { // while !isGameOver
-            synchronized (lock) {
-                while (movingCount >= MAX_CONCURRENT_MOVES) {
-                    try {
-                        // Aguarda se já existirem MAX_CONCURRENT_MOVES obstáculos em movimento
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                movingCount++; // Incrementa o contador de obstáculos em movimento
-            }
-
-            // Lógica de movimento do obstáculo
-            moveObstacle();
-
-            synchronized (lock) {
-                movingCount--; // Decrementa o contador de obstáculos em movimento
-                lock.notifyAll(); // Notifica todas as threads
-            }
-        }
-    }
-
-    private void moveObstacle() {
-        Cell c = obstacle.getCurrentCell();
-        board.addGameElement(obstacle);
-        c.removeObstacle();
-        board.setChanged();
-
-        try {
-            sleep(2000); // Nova Constante no Board OU playerplayinterval?
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void run() {
+		while(true){// while !isGameOver
+			Cell c = obstacle.getCurrentCell();
+			board.addGameElement(obstacle);
+			c.removeObstacle();
+			board.setChanged();
+			try {
+				sleep(2000); //nova Constante no Board OU playerplayinterval?
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
